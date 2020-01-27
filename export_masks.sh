66d9649b85888a362ab0b9e48d6499271ac90a1f
Matt Williams <matt@milliams.com>
2020-01-27 15:45:13 +0000

    Convert masks to greyscale

--- export_masks.sh
+++ export_masks.sh
@@ -11,6 +11,8 @@ for f in ${selected}
 do
     filename=$(basename -- "$f")
     stem=${filename:0:-4}
-    inkscape --without-gui --export-png=masks/${stem}_mask.png --export-area-page --export-background=white --export-background-opacity=1.0 --export-id=layer18 --export-id-only ${f}
+    output_filename=masks/${stem}_mask.png
+    inkscape --without-gui --export-png=${output_filename} --export-area-page --export-background=white --export-background-opacity=1.0 --export-id=layer18 --export-id-only ${f}
+    mogrify -colorspace Gray ${output_filename}
 done
 
