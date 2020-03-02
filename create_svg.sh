74192165123f74fe1d80dc0615e8bbfe28b8e661
Lester Hedges <lester.hedges@gmail.com>
2020-03-02 11:47:40 +0000

    More robust variable expansion and handle / character in file name.

--- create_svg.sh
+++ create_svg.sh
@@ -4,5 +4,6 @@ SVG_DIR=filament_picking
 
 for f in ${SVG_DIR}/*.png
 do
-    sed 's/IMAGE_NAME/'${f}'/g' svg_template > ${f:0:-4}.svg
+    echo "Creating SVG for: $f"
+    sed "s#IMAGE_NAME#${f}#g" svg_template > ${f:0:-4}.svg
 done
